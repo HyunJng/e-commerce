863d241af7d186e32ec049a87dddeb759709fde0
HyunJng <khjung1654@gmail.com>
2025-07-18 03:05:50 +0900

    test: 인기 상품 조회 mock API 테스트

--- src/test/java/kr/hhplus/be/server/small/product/controller/ProductControllerTest.java
+++ src/test/java/kr/hhplus/be/server/small/product/controller/ProductControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static kr.hhplus.be.server.mock.ControllerTestFixtures.기본_성공_포맷_검증;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -33,4 +34,16 @@ class ProductControllerTest {
                 .andExpect(jsonPath("$.result.quantity").value(10)); //TODO: 변경 필요
     }
 
+    @Test
+    void 인기판매상품을_조회_성공하면_5개의_상품정보가_조회된다() throws Exception {
+        //when & then
+        기본_성공_포맷_검증(
+                mockMvc.perform(get("/api/v1/products/best"))
+        )
+                .andExpect(jsonPath("$.result", hasSize(5)))
+                .andExpect(jsonPath("$.result[0].id").value(1))
+                .andExpect(jsonPath("$.result[0].name").value("FIRST"))
+                .andExpect(jsonPath("$.result[0].price").value(1000))
+                .andExpect(jsonPath("$.result[4].name").value("FIFTH"));
+    }
 }
