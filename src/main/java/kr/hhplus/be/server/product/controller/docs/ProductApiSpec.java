daff9c17e063c26ba3dcd1b6bf45779b7bfd7eb0
HyunJng <khjung1654@gmail.com>
2025-07-18 03:08:50 +0900

    docs: 인기 상품 조회 swagger 명세 추가

--- src/main/java/kr/hhplus/be/server/product/controller/docs/ProductApiSpec.java
+++ src/main/java/kr/hhplus/be/server/product/controller/docs/ProductApiSpec.java
@@ -4,7 +4,10 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import kr.hhplus.be.server.common.response.CommonResponse;
+import kr.hhplus.be.server.product.controller.dto.BestProductApi;
 import kr.hhplus.be.server.product.controller.dto.ProductDetail;
+
+import java.util.List;
 
 @Tag(name = "Product", description = "상품 관련 API")
 public interface ProductApiSpec {
@@ -12,4 +15,6 @@ public interface ProductApiSpec {
     @Operation(summary = "상품 상세 조회", description = "상품 상세 정보를 조회합니다.")
     CommonResponse<ProductDetail.Response> viewDetail(@Parameter(description = "상품 아이디")Long id);
 
+    @Operation(summary = "인기 판매 상품 조회", description = "인기 판매 상품 5개를 조회합니다.")
+    CommonResponse<List<BestProductApi.Response>> viewBest();
 }
--- src/main/java/kr/hhplus/be/server/product/controller/dto/BestProductApi.java
+++ src/main/java/kr/hhplus/be/server/product/controller/dto/BestProductApi.java
@@ -1,11 +1,14 @@
 package kr.hhplus.be.server.product.controller.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import kr.hhplus.be.server.product.controller.docs.ProductSchemaDescription;
+
 public class BestProductApi {
 
     public record Response(
-            Long id,
-            String name,
-            Integer price
+            @Schema(description = ProductSchemaDescription.id) Long id,
+            @Schema(description = ProductSchemaDescription.name) String name,
+            @Schema(description = ProductSchemaDescription.price) Integer price
     ) {
 
     }
