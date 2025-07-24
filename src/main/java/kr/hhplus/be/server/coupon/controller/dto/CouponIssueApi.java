ba09503ac94a7a1592d687bed9e1251a8e77d4c3
HyunJng <khjung1654@gmail.com>
2025-07-25 02:46:40 +0900

    feat: 쿠폰 발급 구현

--- src/main/java/kr/hhplus/be/server/coupon/controller/dto/CouponIssueApi.java
+++ src/main/java/kr/hhplus/be/server/coupon/controller/dto/CouponIssueApi.java
@@ -2,8 +2,9 @@ package kr.hhplus.be.server.coupon.controller.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import kr.hhplus.be.server.coupon.controller.docs.CouponSchemaDescription;
+import kr.hhplus.be.server.coupon.usecase.IssuedCouponService;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 public class CouponIssueApi {
 
@@ -11,7 +12,9 @@ public class CouponIssueApi {
             @Schema(description = CouponSchemaDescription.userId) Long userId,
             @Schema(description = CouponSchemaDescription.couponId) Long couponId
     ) {
-
+        public IssuedCouponService.Input to() {
+            return new IssuedCouponService.Input(couponId, userId);
+        }
     }
 
     public record Response(
@@ -20,9 +23,19 @@ public class CouponIssueApi {
             @Schema(description = CouponSchemaDescription.couponName) String couponName,
             @Schema(description = CouponSchemaDescription.discountAmount) Long discountAmount,
             @Schema(description = CouponSchemaDescription.discountType) String discountType,
-            @Schema(description = CouponSchemaDescription.startedAt) LocalDateTime startedAt,
-            @Schema(description = CouponSchemaDescription.endAt) LocalDateTime endAt
+            @Schema(description = CouponSchemaDescription.startedAt) LocalDate startedAt,
+            @Schema(description = CouponSchemaDescription.endAt) LocalDate endAt
     ) {
-
+        public static Response from(IssuedCouponService.Output output) {
+            return new Response(
+                    output.id(),
+                    output.couponId(),
+                    output.couponName(),
+                    output.discountAmount(),
+                    output.discountType(),
+                    output.startedAt(),
+                    output.endAt()
+            );
+        }
     }
 }
---
+++ src/main/java/kr/hhplus/be/server/coupon/domain/Coupon.java
@@ -0,0 +1,49 @@
+package kr.hhplus.be.server.coupon.domain;
+
+import jakarta.persistence.*;
+import kr.hhplus.be.server.common.exception.CommonException;
+import kr.hhplus.be.server.common.response.ResultCode;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "coupons")
+public class Coupon {
+    public enum DiscountType {PERCENT, FIXED_AMOUNT}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "discount_amount")
+    private Long discountAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "discount_type")
+    private DiscountType discountType;
+
+    @Column(name = "dates")
+    private Integer dates;
+
+    @Column(name = "total_quantity")
+    private Integer totalQuantity;
+
+    @Column(name = "created_at")
+    private Long createdAt;
+
+    public Coupon(Long id, String name, Long discountAmount, DiscountType discountType, Integer dates, Integer totalQuantity, Long createdAt) {
+        this.id = id;
+        this.name = name;
+        this.discountAmount = discountAmount;
+        this.discountType = discountType;
+        this.dates = dates;
+        this.totalQuantity = totalQuantity;
+        this.createdAt = createdAt;
+    }
+
+    public Coupon() {}
+
+}
