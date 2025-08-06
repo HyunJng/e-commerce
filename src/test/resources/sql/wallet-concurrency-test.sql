9fd0601cab8e04d59c3768b55d9062d1ac4ef85d
HyunJng <khjung1654@gmail.com>
2025-08-07 01:21:46 +0900

    test: 잔액 충전 동시성 테스트 구현

---
+++ src/test/resources/sql/wallet-concurrency-test.sql
@@ -0,0 +1,6 @@
+INSERT INTO users(id, create_at)
+VALUES(1, '2025-07-22 10:00:00');
+
+
+INSERT INTO wallets(user_id, balance, create_at, update_at)
+VALUES (1, 0, '2025-07-22 10:00:00', '2025-07-22 10:00:00');
