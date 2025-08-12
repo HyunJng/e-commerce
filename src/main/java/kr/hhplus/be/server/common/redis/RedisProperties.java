d6495dd98b7e9ec07d7682b70003f2b6a418fdba
HyunJng <khjung1654@gmail.com>
2025-08-12 23:54:27 +0900

    feat: redis 설정

---
+++ src/main/java/kr/hhplus/be/server/common/redis/RedisProperties.java
@@ -0,0 +1,15 @@
+package kr.hhplus.be.server.common.redis;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "spring.data.redis")
+public class RedisProperties {
+    private String host;
+    private String port;
+    private String timeout;
+}
