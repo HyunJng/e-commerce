5b442e685af50059d44d40113778155bbb2f5574
HyunJng <khjung1654@gmail.com>
2025-08-14 01:17:05 +0900

    chore: Scheduling 실행여부 profile별 분기 처리

--- src/main/java/kr/hhplus/be/server/common/scheduler/SchedulerConfig.java
+++ src/main/java/kr/hhplus/be/server/common/scheduler/SchedulerConfig.java
@@ -1,8 +1,12 @@
 package kr.hhplus.be.server.common.scheduler;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ConditionalOnProperty(
+        name = "app.scheduling.enabled", havingValue = "true"
+)
 @Configuration
 @EnableScheduling
 public class SchedulerConfig {
