8e85abda9c224dee819f88c3bf4e1f4318213128
HyunJng <khjung1654@gmail.com>
2025-09-12 15:03:46 +0900

    refact: 주문결제 잔액 차감 조건부 update로 변경

--- src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
@@ -7,9 +7,10 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "E001", "유효하지 않은 요청(%1)"),
     NOT_FOUND_RESOURCE(HttpStatus.BAD_REQUEST, "E404", "존재하지 않는 %1"),
-    NOT_ENOUGH_PRODUCT_QUANTITY(HttpStatus.BAD_REQUEST, "E405", "%1 상품 재고 부족"),
     INVALID_POLICY(HttpStatus.BAD_REQUEST, "E405", "정책 위반(%1)"),
     FAIL_LOCK_CONFLICT(HttpStatus.CONFLICT, "E406", "데이터 충돌 발생"),
+    NOT_ENOUGH_PRODUCT_QUANTITY(HttpStatus.BAD_REQUEST, "E407", "%1 상품 재고 부족"),
+    NOT_ENOUGH_WALLET_BALANCE(HttpStatus.BAD_REQUEST, "E408", "잔액 부족"),
     FAIL_LOCK_ACQUIRED_TIME(HttpStatus.INTERNAL_SERVER_ERROR, "E502", "락 획득 시간 초과"),
     RACE_CONDITION_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "E501", "잠시후 다시 요청해주세요"),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E999", "미지정 오류(관리자 연락 필요)");
